205c326ea61926029127ddfac57cb51cb0bce098
yueming <crazydogazrael@gmail.com>
2015-10-08 18:46:02 +0800

    add Delete function to binary search tree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -14,20 +14,19 @@ var BinarySearchTree = function (compare) {
     }
 }
 
+function addNode(parentNode, pointer, content) {
+    var newNode = new TreeNode(content);
+    parentNode[pointer] = newNode;
+    this.size += 1;
+    return newNode;
+}
+
 BinarySearchTree.prototype.put = function (content, currentNode) {
     var self = this;
-
     if (!self.root) {
         self.root = new TreeNode(content);
         self.size += 1;
         return;
-    }
-
-    function addNode(parentNode, pointer, content) {
-        var newNode = new TreeNode(content);
-        parentNode[pointer] = newNode;
-        self.size += 1;
-        return newNode;
     }
 
     var currentNode = currentNode || self.root;
@@ -73,16 +72,46 @@ BinarySearchTree.prototype.get = function (key, currentNode) {
         }
     }
 }
-BinarySearchTree.prototype.delete = function (key, currentNode, parentNode) {
-    var current = currentNode || this.root;
+BinarySearchTree.prototype.delete = function (key, currentNode, parentNode, parentLink) {
+    // 删除节点
+    var self = this;
+    var current = currentNode || self.root;
     if (!current) {
         return null;
     }
 
-    var compareResult = this.compare(key, currentNode.content.key);
+    var compareResult = self.compare(key, currentNode.content.key);
     if (compareResult === 0) {
-        
+        var childrenNodeNum = 0;
+        childrenNodeNum += currentNode.left ? 1 : 0;
+        childrenNodeNum += currentNode.right ? 1 : 0;
+
+        var removeNode = function () {
+        }
+
+        switch (childrenNodeNum) {
+            case 0:
+                if (parentNode) {
+                    parentNode[parentLink] = null;
+                } else {
+                    self.root = null;
+                }
+                break;
+            case 1:
+                if (parentNode) {
+                    parentNode[parentLink] = currentNode.left || currentNode.right;
+                } else {
+                    self.root = currentNode.left || currentNode.right;
+                }
+                break;
+            case 2:
+                break;
+        }
+        self.size -= 1;
     } else if (compareResult === -1) {
+        self.delete(key, currentNode.left, currentNode, 'left');
+    } else if (compareResult === 1) {
+        self.delete(key, currentNode.right, currentNode, 'right');
     }
 }
 BinarySearchTree.prototype.getMin = function (currentNode) {
